91af79da5385d34f40c5c2afceec655dda667cab
LuanLuizLLs <luanluiz.dev@gmail.com>
2024-05-01 12:17:40 -0300

    fix: echo text

--- crm/index.php
+++ crm/index.php
@@ -6,22 +6,22 @@ use NoISP\models\ContratoModel;
 use NoISP\models\UsuarioModel;
 use NoISP\models\LeadModel;
 
-echo '<h2>Sem o Princípio da Segregação de Interface</h2>';
+echo "<h2>Sem o Princípio da Segregação de Interface</h2>";
 
-echo '<h3>CRM</h3>';
+echo "<h3>CRM</h3>";
 
 $contratoModel = new ContratoModel();
 $usuarioModel = new UsuarioModel();
 $leadModel = new LeadModel();
 
-echo '<pre>';
+echo "<pre>";
 var_dump($contratoModel);
-echo '</pre>';
+echo "</pre>";
 
-echo '<pre>';
+echo "<pre>";
 var_dump($usuarioModel);
-echo '</pre>';
+echo "</pre>";
 
-echo '<pre>';
+echo "<pre>";
 var_dump($leadModel);
-echo '</pre>';
+echo "</pre>";
